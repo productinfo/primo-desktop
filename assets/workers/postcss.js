1bf270c5af02fd805f16305613955ec65121124a
Mateo Morris <accounts@mateomorris.com>
2020-08-07 19:55:34 -0400

    add script for packaging

--- assets/workers/postcss.js
+++
@@ -1,7 +0,0 @@
-var registerPromiseWorker = require('promise-worker/register');
-
-registerPromiseWorker(async function ({html, css, options, onsuccess}) {
-  const {processPostCSS} = require('primo-functions').default
-  const response = await processPostCSS(css, html, options, onsuccess) 
-  return response;
-});
