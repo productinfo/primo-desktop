6a24a318fa5979da8ee2160e2e965def6e00c23c
Mateo Morris <accounts@mateomorris.com>
2020-08-17 02:44:03 -0400

    .

--- source/src/preview.ts
+++ source/src/preview.ts
@@ -7,18 +7,15 @@ const preview = params.get('preview')
 const previewId = params.get('page')
 
 if (preview === 'multiple') {
-	console.log('single')
 	app = new MultiPreview({
 		target: document.body
 	});
 } else if (preview === 'single' && previewId) {
-	console.log('single')
   app = new SinglePreview({
     target: document.body,
     props: { previewId },
   })
 } else if (preview === 'single') {
-	console.log('component')
 	app = new ComponentPreview({
 		target: document.body
 	});
