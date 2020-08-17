10952d3f586297f5c9ca18a6cdabf78a0e845583
Mateo Morris <accounts@mateomorris.com>
2020-08-16 22:18:28 -0400

    clean up

--- source/public/worker.js
+++
@@ -1,7 +0,0 @@
-importScripts('https://npmcdn.com/promise-worker/dist/promise-worker.register.js');
-
-registerPromiseWorker(async function ({html, css, options, onsuccess}) {
-  const {processPostCSS} = require('primo-functions').default
-  const response = await processPostCSS(css, html, options, onsuccess) 
-  return response;
-});
