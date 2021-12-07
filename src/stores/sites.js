7283bd9a8f5ea95c0be02540d8f4c4e7ff309f05
Dark-Developer93 <eng.abdullahhasan2016@gmail.com>
2021-12-07 19:44:35 +0200

    refactor: change how to handle empty value

--- src/stores/sites.js
+++ src/stores/sites.js
@@ -1,43 +1,42 @@
 import { writable } from 'svelte/store'
 import { browser } from '$app/env'
-import {get,set} from 'idb-keyval'
+import { get, set } from 'idb-keyval'
 import _find from 'lodash/find'
 
-const store = writable([]);
+const store = writable([])
 
 if (browser) initializSiteData()
 
 async function initializSiteData() {
-  const {data} = window.primo // preload.cjs
+  const { data } = window.primo // preload.cjs
   const sites = data.load()
   const sitesDB = await get('sites')
 
-  const rebuiltSites = sites.map(site => {
+  const rebuiltSites = sites.map((site) => {
     const savedSite = _find(sitesDB, (site) => site.data.id === site.id)
     return {
       id: site.id,
       name: site.name,
-      deployments: savedSite.deployments || [],
-      activeDeployment: savedSite.activeDeployment || null,
-      data: site
+      deployments: savedSite ? savedSite.deployments : [],
+      activeDeployment: savedSite ? savedSite.activeDeployment : null,
+      data: site,
     }
   })
 
   store.set(rebuiltSites)
 
-  store.subscribe(s => {
-    const sitesData = s.map(site => ({
-      ...site.data
+  store.subscribe((s) => {
+    const sitesData = s.map((site) => ({
+      ...site.data,
     }))
-    data.save(sitesData) 
+    data.save(sitesData)
 
     set('sites', s)
   })
 }
 
-
 export default {
   update: store.update,
   set: store.set,
-  subscribe: store.subscribe
+  subscribe: store.subscribe,
 }
